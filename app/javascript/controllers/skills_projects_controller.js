7e935951f6bd082c331eddd4f92aea0d775705e9
Louise Giraud <louisegiraud7@gmail.com>
2024-03-13 16:53:42 +0100

    toggle skills projects

--- app/javascript/controllers/skills_projects_controller.js
+++ app/javascript/controllers/skills_projects_controller.js
@@ -1,8 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["skillsTitle", "skillsDetails", "projectsTitle", "projectsDetails"]
 
   connect() {
     console.log("coucou2")
+  }
+
+  toggleskills() {
+    if ((this.skillsTitleTarget.innerHTML.includes("<h2>Skills</h2><h2>⌵</h2>")) && (this.skillsDetailsTarget.style.display = "none")) {
+      this.skillsTitleTarget.innerHTML = "<h2>Skills</h2>";
+      this.skillsDetailsTarget.style.display = "block";
+    } else {
+      this.skillsTitleTarget.innerHTML = "<h2>Skills</h2><h2>⌵</h2>";
+      this.skillsDetailsTarget.style.display = "none"
+    }
+  }
+
+  toggleprojects(event) {
+    if ((this.projectsTitleTarget.innerHTML.includes("<h2>My projects</h2><h2>⌵</h2>")) && (this.projectsDetailsTarget.style.display = "none")) {
+      this.projectsTitleTarget.innerHTML = "<h2>My projects</h2>";
+      this.projectsDetailsTarget.style.display = "flex";
+    } else {
+      this.projectsTitleTarget.innerHTML = "<h2>My projects</h2><h2>⌵</h2>";
+      this.projectsDetailsTarget.style.display = "none"
+    }
   }
 }
