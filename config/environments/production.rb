0cc0da23d0da5014b1c49a2dfb0e93d43aba9ea2
Louise Giraud <louisegiraud7@gmail.com>
2024-03-26 11:33:19 +0100

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'louise-giraud-portfolio-0af1f4ea659e.herokuapp.com/',
+  :domain         => 'louise-giraud.com/',
   :authentication => :plain,
 
   }
