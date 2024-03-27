b974669fc4e4b65b0476a523c3b91caf7cae56be
Louise Giraud <louisegiraud7@gmail.com>
2024-03-27 15:39:17 +0100

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+   api_key: ENV['MAILGUN_API_KEY'],
+   domain: 'louise-giraud.com',
+  }
 end
