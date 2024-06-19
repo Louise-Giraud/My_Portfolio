0b9af9d5837121664be9ea65bfc935e8394691e2
Louise Giraud <louisegiraud7@gmail.com>
2024-06-19 12:48:37 +0200

    cleaning dependencies

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,3 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link popper.js
-//= link bootstrap.min.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "typed.js", to: "https://ga.jspm.io/npm:typed.js@2.1.0/dist/typed.module.js"
